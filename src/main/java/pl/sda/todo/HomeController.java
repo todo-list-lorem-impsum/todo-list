1bb3e6dea782e24c382c893dfb24d2e7e65da726
tomic <violeticus@gmail.com>
2020-09-19 13:23:18 +0200

    Modify home controller

--- src/main/java/pl/sda/todo/HomeController.java
+++ src/main/java/pl/sda/todo/HomeController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping(value="/", method = RequestMethod.GET)
+    @RequestMapping(value="/",
+                    method = RequestMethod.GET)
     public String index() {
         return "index";
     }
